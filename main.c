ec4ee1ecf361a225caccd2a119d8e21b51ec3ab8
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-10-31 19:49:09 +0200

    feat(tuples): added tuples and tuple operations #39 #40 #41

--- main.c
+++ main.c
@@ -1,50 +1,280 @@
 #include "minirt.h"
 
-static int	ft_error(int error_code)
+static void	ft_error(int error_code)
 {
 	exit(error_code);
 }
 
+static void	init_system(t_system *sys, const t_data *data)
+{
+	*sys = (t_system){0};
+	sys->state = DRAFT_MODE;
+	sys->exit_code = 0;
+	if (data)
+	{
+		sys->amb_light = data->amb_light;
+	}
+}
+
+static void	draft_transformations(t_system *sys)
+{
+	(void)sys;
+}
+
+static void	render_scene(t_system *sys, mlx_image_t *img)
+{
+	uint32_t	color;
+	int			x;
+	int			y;
+
+	y = 0;
+	color = sys->amb_light.rgb;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+			mlx_put_pixel(img, ++x, y, color);
+		++y;
+	}
+	sys->state |= RENDER_COMPLETE;
+}
+
+static int	cleanup(t_system *sys)
+{
+	return (sys->exit_code);
+}
+
+static void	frame(void *param)
+{
+	t_app		*app;
+	t_system	*system;
+
+	app = (t_app *)param;
+	system = &app->system;
+	if (mlx_is_key_down(app->mlx, MLX_KEY_ESCAPE))
+		system->state |= SHOULD_EXIT;
+	if (!(system->state & SHOULD_EXIT))
+	{
+		if (system->state & DRAFT_MODE)
+			draft_transformations(system);
+		if (!(system->state & RENDER_COMPLETE))
+			render_scene(system, app->img);
+	}
+	else
+	{
+		mlx_close_window(app->mlx);
+	}
+}
+
 int	main(int argc, char **av)
 {
-	mlx_t		*mlx;
-	mlx_image_t	*img;
-	t_data		data;	
+	t_data	parsed;
+	t_app	app;
+	int		code;
 
-	if(argc != 2)
+	app = (t_app){};
+	parsed = (t_data){};
+	if (argc != 2)
 		ft_error(1);
-
-	rt_parser(av[1], &data);
-	printf("A: %f (%d)\n", data.amb_light.range, data.amb_light.rgb);
-	mlx = mlx_init(WIDTH, HEIGHT, "MiniRT", true);
-	if (!mlx)
+	rt_parser(av[1], &parsed);
+	printf("A: %f (%u)\n", parsed.amb_light.range, parsed.amb_light.rgb);
+	app.mlx = mlx_init(WIDTH, HEIGHT, "MiniRT", true);
+	if (!app.mlx)
 		ft_error(1);
-	img = mlx_new_image(mlx, WIDTH, HEIGHT);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+	app.img = mlx_new_image(app.mlx, WIDTH, HEIGHT);
+	if (!app.img || (mlx_image_to_window(app.mlx, app.img, 0, 0) < 0))
 		ft_error(1);
-	
-	mlx_put_pixel(img, WIDTH * 0.25, HEIGHT * 0.25, data.amb_light.rgb);
-	mlx_put_pixel(img, WIDTH * 0.25, HEIGHT * 0.75, data.amb_light.rgb);
-	mlx_put_pixel(img, WIDTH * 0.5, HEIGHT * 0.5, data.amb_light.rgb);
-	mlx_put_pixel(img, WIDTH * 0.75, HEIGHT * 0.25, data.amb_light.rgb);
-	mlx_put_pixel(img, WIDTH * 0.75, HEIGHT * 0.75, data.amb_light.rgb);
-
-	//mlx_loop_hook(mlx, ft_hook, mlx);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-
-	return (0);
+	init_system(&app.system, &parsed);
+	mlx_loop_hook(app.mlx, frame, &app);
+	mlx_loop(app.mlx);
+	code = cleanup(&app.system);
+	mlx_terminate(app.mlx);
+	return (code);
 }
 
-t_system	system;
-system = (t_system){};
-
-init_system(&system);
-if(!(state & SHOULD_EXIT))
+t_tuple	create_point(float x, float y, float z)
 {
-	if(state & DRAFT_MODE)
-		draft_transformations(&system);
-	if(!(state & RENDER_COMPLETE))
-		render(&system);
+	t_tuple	point;
+
+	point.w = POINT;
+	point.x = x;
+	point.y = y;
+	point.z = z;
+	return (point);
 }
-return(cleanup(&system);
+
+t_tuple	create_vector(float x, float y, float z)
+{
+	t_tuple	vector;
+
+	vector.w = VECTOR;
+	vector.x = x;
+	vector.y = y;
+	vector.z = z;
+	return (vector);
+}
+
+bool	is_float_equal(float a, float b)
+{
+	return (fabsf(a - b) < EPSILON);
+}
+
+static int	classify_w(const t_tuple *t)
+{
+	if (is_float_equal(t->w, 0.0f))
+		return (VECTOR);
+	if (is_float_equal(t->w, 1.0f))
+		return (POINT);
+	return (TUPLE_INCORRECT);
+}
+
+static float	canonical_w(int kind)
+{
+	if (kind == VECTOR)
+		return (0.0f);
+	if (kind == POINT)
+		return (1.0f);
+	return ((float)TUPLE_INCORRECT);
+}
+
+static int	add_kind(int ak, int bk)
+{
+	if (ak == POINT && bk == POINT)
+		return (TUPLE_INCORRECT);
+	return (ak | bk);
+}
+
+static int	sub_kind(int ak, int bk)
+{
+	if (ak == VECTOR && bk == POINT)
+		return (TUPLE_INCORRECT);
+	return (ak ^ bk);
+}
+
+bool	is_tuple_equal(const t_tuple *a, const t_tuple *b)
+{
+	return (is_float_equal(a->w, b->w) && is_float_equal(a->x, b->x)
+		&& is_float_equal(a->z, b->z) && is_float_equal(a->y, b->y));
+}
+
+t_tuple	add_tuple(const t_tuple *a, const t_tuple *b)
+{
+	t_tuple	result;
+	int		ak;
+	int		bk;
+	int		rk;
+
+	ak = classify_w(a);
+	bk = classify_w(b);
+	rk = add_kind(ak, bk);
+	result = (t_tuple){};
+	result.x = a->x + b->x;
+	result.y = a->y + b->y;
+	result.z = a->z + b->z;
+	result.w = canonical_w(rk);
+	return (result);
+}
+
+t_tuple	subtract_tuple(const t_tuple *a, const t_tuple *b)
+{
+	t_tuple	result;
+	int		ak;
+	int		bk;
+	int		rk;
+
+	ak = classify_w(a);
+	bk = classify_w(b);
+	rk = sub_kind(ak, bk);
+	result = (t_tuple){};
+	result.x = a->x - b->x;
+	result.y = a->y - b->y;
+	result.z = a->z - b->z;
+	result.w = canonical_w(rk);
+	return (result);
+}
+
+t_tuple	negate_tuple(const t_tuple *a)
+{
+	t_tuple	result;
+
+	result.x = a->x * -1;
+	result.y = a->y * -1;
+	result.z = a->z * -1;
+	result.w = a->w;
+	return (result);
+}
+
+t_tuple	multiply_tuple(t_tuple *a, float mult)
+{
+	t_tuple	result;
+
+	result = (t_tuple){.x = a->x * mult, .y = a->y * mult, .z = a->z * mult,
+		.w = a->w};
+	return (result);
+}
+
+t_tuple	divide_tuple(t_tuple *a, float div)
+{
+	t_tuple	result;
+
+	result = (t_tuple){};
+	result.w = TUPLE_INCORRECT;
+	if (fabsf(div) < EPSILON)
+		return (result);
+	result = (t_tuple){.x = a->x / div, .y = a->y / div, .z = a->z / div,
+		.w = a->w};
+	return (result);
+}
+
+float	magnitude_tuple(t_tuple *a)
+{
+	return (sqrtf(a->x * a->x + a->y * a->y + a->z * a->z));
+}
+
+t_tuple	normalize_tuple(t_tuple *a)
+{
+	float	mag;
+	t_tuple	result;
+	int		ak;
+
+	result = (t_tuple){};
+	result.w = TUPLE_INCORRECT;
+	ak = classify_w(a);
+	if (ak != VECTOR)
+		return (result);
+	mag = magnitude_tuple(a);
+	if (mag < EPSILON)
+		return (result);
+	result = (t_tuple){.x = a->x / mag, .y = a->y / mag, .z = a->z / mag,
+		.w = VECTOR};
+	return (result);
+}
+
+float	dot_product_tuple(t_tuple *a, t_tuple *b)
+{
+	int	ak;
+	int	bk;
+
+	ak = classify_w(a);
+	bk = classify_w(b);
+	if (ak != VECTOR || bk != VECTOR)
+		return (0.0f);
+	return (a->x * b->x + a->y * b->y + a->z * b->z);
+}
+
+t_tuple	cross_product_tuple(t_tuple *a, t_tuple *b)
+{
+	t_tuple	result;
+	int		ak;
+	int		bk;
+
+	result = (t_tuple){};
+	result.w = TUPLE_INCORRECT;
+	ak = classify_w(a);
+	bk = classify_w(b);
+	if (ak != VECTOR || bk != VECTOR)
+		return (result);
+	result = (t_tuple){.x = a->y * b->z - a->z * b->y, .y = a->z * b->x - a->x
+		* b->z, .z = a->x * b->y - a->y * b->x, .w = VECTOR};
+	return (result);
+}
--- minirt.h
+++ minirt.h
@@ -1,19 +1,24 @@
 #ifndef MINIRT_H
 # define MINIRT_H
 
-#include <stdint.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "libft/libft.h"
-#include "MLX42/include/MLX42/MLX42.h"
+# include "MLX42/include/MLX42/MLX42.h"
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#define WIDTH 256
-#define HEIGHT 256
+# define WIDTH 256
+# define HEIGHT 256
+# define EPSILON 0.00001
+# define VECTOR 0
+# define POINT 1
+# define TUPLE_INCORRECT 2
 
-
-typedef enum	e_sys_state
+typedef enum e_sys_state
 {
 	DRAFT_MODE = (1 << 0),
 	RENDER_COMPLETE = (1 << 1),
@@ -22,35 +27,42 @@ typedef enum	e_sys_state
 	INIT = (1 << 4),
 }					t_sys_state;
 
-typedef	struct		s_vec3
+typedef struct s_tuple
 {
 	float			x;
 	float			y;
 	float			z;
-}					t_vec3
+	float			w;
+}					t_tuple;
 
-typedef struct		s_plane
+typedef enum e_obj_state
+{
+	ALONE,
+	IN_GROUP,
+}					t_obj_state;
+
+typedef struct s_plane
 {
 	t_obj_state		state;
-	t_vec3			location;
-	t_vec3			rotation;
+	t_tuple			location;
+	t_tuple			rotation;
 	uint32_t		color;
 }					t_plane;
 
-typedef struct		s_ball
+typedef struct s_ball
 {
 	t_obj_state		state;
-	t_vec3			location;
-	t_vec3			rotation;
+	t_tuple			location;
+	t_tuple			rotation;
 	float			diameter;
 	uint32_t		color;
 }					t_ball;
 
-typedef struct		s_cylinder
+typedef struct s_cylinder
 {
 	t_obj_state		state;
-	t_vec3			location;
-	t_vec3			rotation;
+	t_tuple			location;
+	t_tuple			rotation;
 	float			length;
 	float			diameter;
 	t_plane			n_cap;
@@ -58,14 +70,23 @@ typedef struct		s_cylinder
 	uint32_t		color;
 }					t_cylinder;
 
-typedef union		u_object
+typedef enum e_type_flag
 {
-	t_plane,
-	t_ball,
-	t_cylinder,
+	PLANE,
+	BALL,
+	CYLINDER,
+	END
+}					t_type_flag;
+
+typedef union u_object
+{
+	t_type_flag		type;
+	t_plane			plane;
+	t_ball			ball;
+	t_cylinder		cylinder;
 }					t_object;
 
-typedef struct		s_world
+typedef struct s_world
 {
 	t_plane			heaven;
 	t_plane			ground;
@@ -75,30 +96,35 @@ typedef struct		s_world
 	t_plane			south;
 }					t_world;
 
-typedef	struct		s_camera
+typedef struct s_camera
 {
-	t_vec3			location;
-	t_vec3			rotation;
+	t_tuple			location;
+	t_tuple			rotation;
 	t_ball			ball;
 	int				fov;
 }					t_camera;
 
-typedef struct		s_spot_light
+typedef struct s_spot_light
 {
 	double			range;
-	t_vec3			location;
-	t_vec3			rotation;
+	t_tuple			location;
+	t_tuple			rotation;
 	int				size;
 	uint32_t		color;
 }					t_spot_light;
 
-typedef struct		s_amb_light
+typedef struct s_amb_light
 {
 	double			range;
 	uint32_t		rgb;
 }					t_amb_light;
 
-typedef struct		s_system
+typedef struct s_data
+{
+	t_amb_light		amb_light;
+}					t_data;
+
+typedef struct s_system
 {
 	t_sys_state		state;
 	int				exit_code;
@@ -108,11 +134,15 @@ typedef struct		s_system
 	t_amb_light		amb_light;
 	t_spot_light	**light_list;
 	t_world			world;
-	
 }					t_system;
 
+typedef struct s_app
+{
+	mlx_t			*mlx;
+	mlx_image_t		*img;
+	t_system		system;
+}					t_app;
 
-void	rt_parser(char *input, t_data *data);
-
+void				rt_parser(char *input, t_data *data);
 
 #endif
