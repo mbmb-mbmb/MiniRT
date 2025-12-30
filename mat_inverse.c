b4b35787707a8afc621464ca7c30fb2ae974f6f1
mbmb-mbmb <markus.bonsdorff@iki.fi>
2025-12-30 21:10:08 +0200

    fix(error_handling): some cleanup.

--- mat_inverse.c
+++ mat_inverse.c
@@ -12,62 +12,62 @@
 
 #include "minirt.h"
 
-static void	calc_subdets_top(t_mat *m, float sub_top[6])
+static void	calc_subdets_top(t_mat *m, float subdets_01[6])
 {
-	sub_top[0] = m->m[0][0] * m->m[1][1] - m->m[1][0] * m->m[0][1];
-	sub_top[1] = m->m[0][0] * m->m[1][2] - m->m[1][0] * m->m[0][2];
-	sub_top[2] = m->m[0][0] * m->m[1][3] - m->m[1][0] * m->m[0][3];
-	sub_top[3] = m->m[0][1] * m->m[1][2] - m->m[1][1] * m->m[0][2];
-	sub_top[4] = m->m[0][1] * m->m[1][3] - m->m[1][1] * m->m[0][3];
-	sub_top[5] = m->m[0][2] * m->m[1][3] - m->m[1][2] * m->m[0][3];
+	subdets_01[0] = m->m[0][0] * m->m[1][1] - m->m[1][0] * m->m[0][1];
+	subdets_01[1] = m->m[0][0] * m->m[1][2] - m->m[1][0] * m->m[0][2];
+	subdets_01[2] = m->m[0][0] * m->m[1][3] - m->m[1][0] * m->m[0][3];
+	subdets_01[3] = m->m[0][1] * m->m[1][2] - m->m[1][1] * m->m[0][2];
+	subdets_01[4] = m->m[0][1] * m->m[1][3] - m->m[1][1] * m->m[0][3];
+	subdets_01[5] = m->m[0][2] * m->m[1][3] - m->m[1][2] * m->m[0][3];
 }
 
-static void	calc_subdets_bot(t_mat *m, float sub_bot[6])
+static void	calc_subdets_bot(t_mat *m, float subdets_23[6])
 {
-	sub_bot[0] = m->m[2][0] * m->m[3][1] - m->m[3][0] * m->m[2][1];
-	sub_bot[1] = m->m[2][0] * m->m[3][2] - m->m[3][0] * m->m[2][2];
-	sub_bot[2] = m->m[2][0] * m->m[3][3] - m->m[3][0] * m->m[2][3];
-	sub_bot[3] = m->m[2][1] * m->m[3][2] - m->m[3][1] * m->m[2][2];
-	sub_bot[4] = m->m[2][1] * m->m[3][3] - m->m[3][1] * m->m[2][3];
-	sub_bot[5] = m->m[2][2] * m->m[3][3] - m->m[3][2] * m->m[2][3];
+	subdets_23[0] = m->m[2][0] * m->m[3][1] - m->m[3][0] * m->m[2][1];
+	subdets_23[1] = m->m[2][0] * m->m[3][2] - m->m[3][0] * m->m[2][2];
+	subdets_23[2] = m->m[2][0] * m->m[3][3] - m->m[3][0] * m->m[2][3];
+	subdets_23[3] = m->m[2][1] * m->m[3][2] - m->m[3][1] * m->m[2][2];
+	subdets_23[4] = m->m[2][1] * m->m[3][3] - m->m[3][1] * m->m[2][3];
+	subdets_23[5] = m->m[2][2] * m->m[3][3] - m->m[3][2] * m->m[2][3];
 }
 
-static void	build_inverse_matrix(t_mat *inv, t_mat *m, float s[6], float c[6], float inv_det)
+static void	build_inverse_matrix(t_mat *inv, t_mat *m, float t[6], float b[6], float inv_det)
 {
 	inv->type = FOUR;
-	inv->m[0][0] = (m->m[1][1] * c[5] - m->m[1][2] * c[4] + m->m[1][3] * c[3]) * inv_det;
-	inv->m[0][1] = (-m->m[0][1] * c[5] + m->m[0][2] * c[4] - m->m[0][3] * c[3]) * inv_det;
-	inv->m[0][2] = (m->m[3][1] * s[5] - m->m[3][2] * s[4] + m->m[3][3] * s[3]) * inv_det;
-	inv->m[0][3] = (-m->m[2][1] * s[5] + m->m[2][2] * s[4] - m->m[2][3] * s[3]) * inv_det;
-	inv->m[1][0] = (-m->m[1][0] * c[5] + m->m[1][2] * c[2] - m->m[1][3] * c[1]) * inv_det;
-	inv->m[1][1] = (m->m[0][0] * c[5] - m->m[0][2] * c[2] + m->m[0][3] * c[1]) * inv_det;
-	inv->m[1][2] = (-m->m[3][0] * s[5] + m->m[3][2] * s[2] - m->m[3][3] * s[1]) * inv_det;
-	inv->m[1][3] = (m->m[2][0] * s[5] - m->m[2][2] * s[2] + m->m[2][3] * s[1]) * inv_det;
-	inv->m[2][0] = (m->m[1][0] * c[4] - m->m[1][1] * c[2] + m->m[1][3] * c[0]) * inv_det;
-	inv->m[2][1] = (-m->m[0][0] * c[4] + m->m[0][1] * c[2] - m->m[0][3] * c[0]) * inv_det;
-	inv->m[2][2] = (m->m[3][0] * s[4] - m->m[3][1] * s[2] + m->m[3][3] * s[0]) * inv_det;
-	inv->m[2][3] = (-m->m[2][0] * s[4] + m->m[2][1] * s[2] - m->m[2][3] * s[0]) * inv_det;
-	inv->m[3][0] = (-m->m[1][0] * c[3] + m->m[1][1] * c[1] - m->m[1][2] * c[0]) * inv_det;
-	inv->m[3][1] = (m->m[0][0] * c[3] - m->m[0][1] * c[1] + m->m[0][2] * c[0]) * inv_det;
-	inv->m[3][2] = (-m->m[3][0] * s[3] + m->m[3][1] * s[1] - m->m[3][2] * s[0]) * inv_det;
-	inv->m[3][3] = (m->m[2][0] * s[3] - m->m[2][1] * s[1] + m->m[2][2] * s[0]) * inv_det;
+	inv->m[0][0] = (m->m[1][1] * b[5] - m->m[1][2] * b[4] + m->m[1][3] * b[3]) * inv_det;
+	inv->m[0][1] = (-m->m[0][1] * b[5] + m->m[0][2] * b[4] - m->m[0][3] * b[3]) * inv_det;
+	inv->m[0][2] = (m->m[3][1] * t[5] - m->m[3][2] * t[4] + m->m[3][3] * t[3]) * inv_det;
+	inv->m[0][3] = (-m->m[2][1] * t[5] + m->m[2][2] * t[4] - m->m[2][3] * t[3]) * inv_det;
+	inv->m[1][0] = (-m->m[1][0] * b[5] + m->m[1][2] * b[2] - m->m[1][3] * b[1]) * inv_det;
+	inv->m[1][1] = (m->m[0][0] * b[5] - m->m[0][2] * b[2] + m->m[0][3] * b[1]) * inv_det;
+	inv->m[1][2] = (-m->m[3][0] * t[5] + m->m[3][2] * t[2] - m->m[3][3] * t[1]) * inv_det;
+	inv->m[1][3] = (m->m[2][0] * t[5] - m->m[2][2] * t[2] + m->m[2][3] * t[1]) * inv_det;
+	inv->m[2][0] = (m->m[1][0] * b[4] - m->m[1][1] * b[2] + m->m[1][3] * b[0]) * inv_det;
+	inv->m[2][1] = (-m->m[0][0] * b[4] + m->m[0][1] * b[2] - m->m[0][3] * b[0]) * inv_det;
+	inv->m[2][2] = (m->m[3][0] * t[4] - m->m[3][1] * t[2] + m->m[3][3] * t[0]) * inv_det;
+	inv->m[2][3] = (-m->m[2][0] * t[4] + m->m[2][1] * t[2] - m->m[2][3] * t[0]) * inv_det;
+	inv->m[3][0] = (-m->m[1][0] * b[3] + m->m[1][1] * b[1] - m->m[1][2] * b[0]) * inv_det;
+	inv->m[3][1] = (m->m[0][0] * b[3] - m->m[0][1] * b[1] + m->m[0][2] * b[0]) * inv_det;
+	inv->m[3][2] = (-m->m[3][0] * t[3] + m->m[3][1] * t[1] - m->m[3][2] * t[0]) * inv_det;
+	inv->m[3][3] = (m->m[2][0] * t[3] - m->m[2][1] * t[1] + m->m[2][2] * t[0]) * inv_det;
 }
 
 t_mat	invert_matrix(t_mat *m)
 {
 	t_mat	inv;
 	float	det;
-	float	sub_top[6];
-	float	sub_bot[6];
+	float	subdets_01[6];
+	float	subdets_23[6];
 
-	calc_subdets_top(m, sub_top);
-	calc_subdets_bot(m, sub_bot);
-	det = sub_top[0] * sub_bot[5] - sub_top[1] * sub_bot[4] + sub_top[2] * sub_bot[3]
-	+ sub_top[3] * sub_bot[2] - sub_top[4] * sub_bot[1] + sub_top[5] * sub_bot[0];
+	calc_subdets_top(m, subdets_01);
+	calc_subdets_bot(m, subdets_23);
+	det = subdets_01[0] * subdets_23[5] - subdets_01[1] * subdets_23[4] + subdets_01[2] * subdets_23[3]
+	+ subdets_01[3] * subdets_23[2] - subdets_01[4] * subdets_23[1] + subdets_01[5] * subdets_23[0];
 	if (is_float_zero(det))
 		return (create_identity_matrix(4));
 	det = 1.0f / det;
-	build_inverse_matrix(&inv, m, sub_top, sub_bot, det);
+	build_inverse_matrix(&inv, m, subdets_01, subdets_23, det);
 	set_matrix_dim(&inv, 4);
 	return (inv);
 }
