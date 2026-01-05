7259f08607cd5b9a29f3e873870cf67642706eb3
Jyry Niemitalo <jyniemit@student.hive.fi>
2026-01-05 14:14:58 +0200

    refactor(app+system): split into one function per file

---
+++ system/prepare_scene.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prepare_scene.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbonsdor <mbonsdor@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/26 16:08:34 by mbonsdor          #+#    #+#             */
+/*   Updated: 2026/01/02 11:43:12 by mbonsdor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minirt.h"
+
+void	prepare_scene(t_system *sys)
+{
+	int	i;
+
+	camera_transform(&sys->camera);
+	init_canvas_dimensions(&sys->camera, WIDTH);
+	i = 0;
+	while (i < sys->object_count)
+	{
+		if (sys->obj_list[i].type == SPHERE)
+			setup_sphere_transform(&sys->obj_list[i]);
+		else if (sys->obj_list[i].type == PLANE)
+			transform_plane(&sys->obj_list[i]);
+		else if (sys->obj_list[i].type == CYLINDER)
+			transform_cylinder(&sys->obj_list[i]);
+		i++;
+	}
+}
