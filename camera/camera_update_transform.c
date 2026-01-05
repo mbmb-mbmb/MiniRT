5d889e19229c509733d14eacaa4ebed3b7225b88
Jyry Niemitalo <jyniemit@student.hive.fi>
2026-01-05 14:58:29 +0200

    fix(norme): some norm fixes.

--- camera/camera_update_transform.c
+++ camera/camera_update_transform.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   camera_update_transform.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbonsdor <mbonsdor@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/26 16:08:34 by mbonsdor          #+#    #+#             */
-/*   Updated: 2026/01/02 11:43:12 by mbonsdor         ###   ########.fr       */
+/*   Created: 2026/01/05 14:21:22 by jyniemit          #+#    #+#             */
+/*   Updated: 2026/01/05 14:21:25 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
