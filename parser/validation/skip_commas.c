82f108be5d4bfed2c74b29c7934f4854b82718c5
mbmb-mbmb <markus.bonsdorff@iki.fi>
2026-01-07 11:54:31 +0200

    fix(final_fixes): modified skip_commas() to not accept garbage

--- parser/validation/skip_commas.c
+++ parser/validation/skip_commas.c
@@ -17,9 +17,11 @@ int	skip_commas(char *buffer, t_system *sys)
 	int	i;
 
 	i = 0;
-	while (buffer[i] && buffer[i] != ',' && buffer[i] != '\n')
+	while (buffer[i] && (buffer[i] == ' ' || buffer[i] == '\t'))
 		i++;
 	if (buffer[i] != ',')
 		error_exit("Expected comma between components.\n", sys);
+	while (buffer[i] && (buffer[i] == ' ' || buffer[i] == '\t'))
+		i++;
 	return (i + 1);
 }
